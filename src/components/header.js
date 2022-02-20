b9a46257eb0d8c1e8ad58670b9eaac65b0974c37
Andrés Cañón <mario.canon@datatools.com.co>
2022-02-20 14:08:54 -0500

    PropTypes Header component.

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,20 @@
-const Header = () => {
+import PropTypes from "prop-types"
+
+const Header = ( {title} ) => {
   return (
       <div className="header">
-          <h5>¡Hello! The Awesome Task Tracker Here!</h5>
+          <h5> {title} </h5>
           <button className="btn">Add Task</button>
       </div>
   )
+}
+
+Header.defaultProps = {
+    title: `¡Hello! The Awesome Task Tracker Here!`,
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
