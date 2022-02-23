ee323551fc6a024b18ffbe61180672a6040bfdd7
Andrés Cañón <mario.canon@datatools.com.co>
2022-02-22 21:04:07 -0500

    Task-Tasks-FontAwesome

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import Header from "./components/header";
+import Tasks from "./components/tasks";
 
 function App() {
-
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Task number 1.',
+        day: 'Monday at 8:00AM',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Task number 2.',
+        day: 'Monday at 9:00AM',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Task number 3.',
+        day: 'Monday at 10:00AM',
+        reminder: true
+    }
+])
   const appName = `Task Tracker`;
   const appDescription = `A funny React App in the stack!`;
 
@@ -10,6 +31,7 @@ function App() {
       <h1>React {appName}</h1>
       <small> {appDescription} </small>
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
