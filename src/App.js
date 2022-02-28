8190d750f1127670c7b94d74598323f683700ac5
Andrés Cañón <mario.canon@datatools.com.co>
2022-02-28 09:58:33 -0500

    JSON Server - useEffect Hook & Fetch tasks.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddTask from "./components/addtask";
 import Header from "./components/header";
 import Tasks from "./components/tasks";
 
 function App() {
-  const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Task number 1.',
-      day: 'Monday at 8:00AM',
-      reminder: true
-    },
-    {
-      id: 2,
-      text: 'Task number 2.',
-      day: 'Monday at 9:00AM',
-      reminder: true
-    },
-    {
-      id: 3,
-      text: 'Task number 3.',
-      day: 'Monday at 10:00AM',
-      reminder: true
-    }
-  ])
   const appName = `Task Tracker`;
   const appDescription = `A funny React App in the stack!`;
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks();
+  }, []);
+  
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    return data;
+  }
 
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000 + 1);
